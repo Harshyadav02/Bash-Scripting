378850fc5e7234fabb7df63ee6da9f37e9090230
harshyadav02 <hy043768@gmail.com>
2023-07-27 21:21:00 +0530

    Creating variables

---
+++ 02_Variables/01_variables.sh
@@ -0,0 +1,14 @@
+#Shell variables are created once they are assigned a value. A variable can contain a number, a character or a string of characters. Variable name is case sensitive and can consist of a combination of letters and the underscore "_". Value assignment is done using the "=" sign. Note that no space permitted on either side of = sign when initializing variables.
+
+# Note :- use $ before the variable while printing the variable 
+
+x=5;
+MyFirstLetters=ABC;
+greeting='Hello        world!';
+echo $x, $MyFirstLetters, $greeting;
+
+
+y=10;
+echo "The value of y is :  $y";
+
+

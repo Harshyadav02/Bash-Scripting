7992dcd013787b48d270f09de417bc89ab465daa
harshyadav02 <hy043768@gmail.com>
2023-07-27 21:20:05 +0530

    First program in Bash

---
+++ 01_Intoduction/02_hello_world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+#!/bin/bash at the beginning of a Bash script, you are telling the system to use the Bash shell to interpret and execute the commands in the script.
+
+# echo is used to print any statment
+echo "hello world"
+
+

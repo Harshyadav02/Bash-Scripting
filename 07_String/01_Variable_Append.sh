1689923a5776ab2f215a94c5f948322ce13c7653
harshyadav02 <hy043768@gmail.com>
2023-07-29 19:17:35 +0530

    Giving variable as a string

---
+++ 07_String/01_Variable_Append.sh
@@ -0,0 +1,11 @@
+# Simple variable append
+
+#Declare two string variables in the bash script printed to console using echo by appending(enclosed in a double quote with variable syntax)
+
+string1='Harsh '
+string2='Yadav'
+echo "$string1 $string2" 
+
+# You can also append without a double quote
+
+echo $string1 $string2 

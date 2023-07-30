0192ce721921b0f9349f9ca9e7703ea4569aa436
harshyadav02 <hy043768@gmail.com>
2023-07-30 18:55:02 +0530

    Reading file using cat

---
+++ 10_ReadFile/01_usingCat.sh
@@ -0,0 +1,18 @@
+# Reading File Using 'cat fileName'
+
+# We can use the following syntax to take a print of the contents of the file to a terminal.
+
+file=`cat data.txt`;
+
+echo $file ;
+#----------------------------------------------------------------
+
+
+#Reading File Using '$(<fileName>)
+
+value=$(<data.txt)
+
+echo $value ;
+
+#---------------------------------------------------------------- 
+
